dd0aa58f82a41c7d98daac457fa20db4174d9de2
MateoPV74 <mateopereyra1974@gmail.com>
2026-05-14 20:33:47 -0300

    añadida funcionalidad al login

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -2,17 +2,40 @@ import React from 'react'
 import { useState, useRef } from 'react'
 import { Row, Col, Form, Button } from 'react-bootstrap'
 import { Link, useNavigate } from 'react-router-dom'
-const Login = () => {
+const Login = ({onLogin}) => {
 
 const navigate = useNavigate()
 const [email, setEmail] = useState("")
 const [password, setPassword] = useState("")
+const [emailError, setEmailError] = useState(false)
+const [passwordError, setPasswordError] = useState(false)
 
+const emailRef = useRef(null)
+const passwordRef = useRef(null)
 
 const handleSubmit = (e) =>{
     e.preventDefault()
-    console.log(email, password)
-    navigate('/dashboard')
+    if(validation()){
+        console.log(email, password)
+        navigate('/dashboard')
+        onLogin(true)
+    }
+}
+
+const validation = () => {
+    let isValid = true
+    if(password == "") {
+        passwordRef.current.focus()
+        setPasswordError(true)
+        isValid = false
+    }
+    if(email == "") {
+        emailRef.current.focus()
+        setEmailError(true)
+        isValid = false
+    } 
+
+    return(isValid)
 }
 
 
@@ -22,17 +45,26 @@ const handleSubmit = (e) =>{
             <Link className='text-end' to={'/register'}> Registrarse </Link>
             <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="ingrese email" 
-                onChange={(e) => setEmail(e.target.value)}/>
-            <Form.Text className="text-muted">
-                Mensaje de error
+                <Form.Control type="email" placeholder="ingrese email" ref={emailRef} 
+                onChange={(e) => {
+                    setEmail(e.target.value) 
+                    setEmailError(false)
+                }}/>
+            <Form.Text className="text-danger">
+                {emailError && <p>este campo no puede estar vacío </p>}
             </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Contraseña" 
-                onChange={(e) => setPassword(e.target.value)}/>
+                <Form.Control type="password" placeholder="Contraseña" ref={passwordRef}
+                onChange={(e) => {
+                    setPassword(e.target.value)
+                    setPasswordError(false)    
+                }}/>
+                <Form.Text className="text-danger">
+                {passwordError && <p>este campo no puede estar vacío </p>}
+            </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check type="checkbox" label="Mantener sesión" />
