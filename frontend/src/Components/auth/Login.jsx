7fb28621d02f49c031105e2a2ca8e047fb872f10
Julian <julilei276@gmail.com>
2026-06-04 21:59:56 -0300

    Unifico Login y register en un mismo modal

---
+++ frontend/src/Components/auth/Login.jsx
@@ -0,0 +1,206 @@
+import React, { useEffect, useState } from 'react'
+import { Modal, Button, Row, Col } from 'react-bootstrap'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const loginSchema = z
+  .object({
+    username: z.string().min(1, "Nombre de usuario requerido"),
+    password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres")
+  })
+
+const registerSchema = z
+  .object({
+    name: z.string().min(1, "Nombre requerido"),
+    surname: z.string().min(1, "Apellido requerido"),
+    username: z.string().min(1, "Nombre de usuario requerido"),
+    password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres"),
+    email: z.string().email("email invalido")
+  })
+
+const Login = ({show, onHide}) => {
+  const [isLoginMode, setIsLoginMode] = useState(true)
+
+  const {
+    register: registerLogin,
+    handleSubmit: handleSubmitLogin,
+    reset: resetLogin,
+    formState: {errors: errorsLogin}
+  } = useForm({
+    resolver:zodResolver(loginSchema)
+  })
+
+  const {
+    register: registerRegister,
+    handleSubmit: handleSubmitRegister,
+    reset: resetRegister,
+    formState: {errors: errorsRegister}
+  } = useForm({
+    resolver:zodResolver(registerSchema)
+  })
+
+  useEffect(() => {
+    resetLogin()
+    resetRegister()
+  }, [show, isLoginMode, resetLogin, resetRegister])
+
+  const onLoginSubmit = (data) => {
+    console.log('Login data: ', data)
+    onHide()
+  } 
+
+  const onRegisterSubmit = (data) => {
+    console.log('Register data: ', data)
+    onHide()
+  } 
+
+  return (
+    <div>
+      <Modal
+        show={show}
+        onHide={onHide}
+        centered
+        backdrop='static'
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          {isLoginMode ? 'Iniciar Sesion' : 'Crear Cuenta'}
+        </Modal.Header>
+
+        <Modal.Body>
+          {isLoginMode ? (
+            <form id='login' onSubmit={handleSubmitLogin(onLoginSubmit)}>
+              <Row>
+                <Col>
+                  <label className='form-label'>Nombre de Usuario</label>
+                  <input
+                    {...registerLogin("username")}
+                    className='form-control mb-2'
+                    type='text'
+                    placeholder='Username'
+                    />
+                  {errorsLogin.username && (
+                    <p className='text-danger'>{errorsLogin.username.message}</p>
+                  )}
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <label className='form-label'>Contraseña</label>
+                  <input
+                      {...registerLogin("password")}
+                      className='form-control mb-2'
+                      type='password'
+                      placeholder='Contraseña'
+                      />
+                  {errorsLogin.password && (
+                    <p className='text-danger'>{errorsLogin.password.message}</p>
+                  )}
+                </Col>
+              </Row>
+            </form>
+          ) : (
+            <form id='register' onSubmit={handleSubmitRegister(onRegisterSubmit)}>
+              <Row>
+                  <Col>
+                      <label className='form-label'>Username</label>
+                      <input
+                          {...registerRegister("username")}
+                          className='form-control mb-2'
+                          type='text'
+                          placeholder='Username'
+                          />
+                      {errorsRegister.username && (
+                          <p className='text-danger'>{errorsRegister.username.message}</p>
+                      )}
+                  </Col>
+              </Row>
+              <Row>
+                  <Col>
+                      <label className='form-label'>Nombre</label>
+                      <input
+                          {...registerRegister("name")}
+                          className='form-control mb-2'
+                          type='text'
+                          placeholder='Nombre'
+                      />
+                      {errorsRegister.name && (
+                          <p className='text-danger'>{errorsRegister.name.message}</p>
+                      )}
+                  </Col>
+                  <Col>
+                      <label className='form-label'>Apellido</label>
+                      <input
+                          {...registerRegister("surname")}
+                          className='form-control mb-2'
+                          type='text'
+                          placeholder='Apellido'
+                      />
+                      {errorsRegister.surname && (
+                          <p className='text-danger'>{errorsRegister.surname.message}</p>
+                      )}
+                  </Col>
+              </Row>
+              <Row>
+                  <Col>
+                      <label className='form-label'>Email</label>
+                      <input
+                          {...registerRegister("email")}
+                          className='form-control mb-2'
+                          type='text'
+                          placeholder='Email'
+                      />
+                      {errorsRegister.email && (
+                          <p className='text-danger'>{errorsRegister.email.message}</p>
+                      )}
+                  </Col>
+                  <Col>
+                      <label className='form-label'>Contraseña</label>
+                      <input
+                          {...registerRegister("password")}
+                          className='form-control mb-2'
+                          type='password'
+                          placeholder='Contraseña'
+                      />
+                      {errorsRegister.password && (
+                          <p className='text-danger'>{errorsRegister.password.message}</p>
+                      )}
+                  </Col>
+              </Row>
+            </form>
+          )}
+          <div className='text-center mt-3'>
+            <small className='text-muted'>
+              {isLoginMode ? '¿Aún no tienes cuenta?' : '¿Ya tienes cuenta?'}
+              <Button
+                type='button'
+                variant='link'
+                className='p-0 mb-1'
+                style={{ textDecoration: 'none', fontWeight: 'bold' }}
+                onClick={() => setIsLoginMode(!isLoginMode)}
+              >
+                {isLoginMode ? 'Registrate' : 'Inicia sesión'}
+              </Button>
+            </small>
+          </div>
+        </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="danger" onClick={onHide}>
+              Cancelar
+          </Button>
+          <Button
+              variant="primary"
+              type="submit"
+              form={isLoginMode ? 'login' : 'register'}
+          >
+              {isLoginMode ? 'Confirmar' : 'Crear Cuenta'}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
+}
+
+export default Login
